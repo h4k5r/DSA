ba22f01f51808314f76907f8ebad3103c846ccf7
World Ender <hariharansriram17@gmail.com>
2025-03-20 12:09:39 +0000

    complete fibonacci series

--- src/main/java/hari/engineer/Main.java
+++ src/main/java/hari/engineer/Main.java
@@ -4,18 +4,26 @@ public class Main {
     public static void fibonacci_memo(int n) {
         int current = 0;
         int next = 1;
-        for (int i = 0; i < n; i++) {
-            System.out.println(current);
+        for (int i = 1; i <=n; i++) {
+            System.out.println(next);
             int temp = current;
             current = next;
             next = current + temp;
         }
     }
 
-    public static void fibonacci_recursion(int n) {}
+    public static int fibonacci_recursion(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci_recursion(n-1) + fibonacci_recursion(n-2);
+    }
 
     public static void main(String[] args) {
         fibonacci_memo(10);
-        fibonacci_recursion(10);
+        System.out.println(fibonacci_recursion(10));
     }
 }
