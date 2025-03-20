15988fb224dc98f3ec305ea1baabf37bbb770ffa
Hariharan Kurathallvar Sriram <hariharan.kurathallvar-sriram@stfc.ac.uk>
2025-03-20 14:01:10 +0000

    add tribonacci

--- src/main/java/hari/engineer/Main.java
+++ src/main/java/hari/engineer/Main.java
@@ -36,10 +36,30 @@ public class Main {
         memo.put(n, result);
         return result;
     }
+    public static int tribonacci(int n) {
+        return tribonacci(n , new HashMap<>());
+    }
+
+    public static int tribonacci(int n, HashMap<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return 0;
+        } if (n == 2) {
+            return 1;
+        }
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        int result = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+        memo.put(n, result);
+        return result;
+    }
 
     public static void main(String[] args) {
-        fibonacci(10);
-        System.out.println(fibonacci_recursion(10));
-        System.out.println(fibonacci_memo(10));
+//        fibonacci(10);
+//        System.out.println(fibonacci_recursion(10));
+//        System.out.println(fibonacci_memo(10));
+        System.out.println(tribonacci(8));
     }
 }
