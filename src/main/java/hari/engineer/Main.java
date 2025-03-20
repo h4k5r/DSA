3ed1322793825ba2fc2b71f03e392beb6ace6f46
Hariharan Kurathallvar Sriram <hariharan.kurathallvar-sriram@stfc.ac.uk>
2025-03-20 12:39:48 +0000

    add correct memo version

--- src/main/java/hari/engineer/Main.java
+++ src/main/java/hari/engineer/Main.java
@@ -1,10 +1,12 @@
 package hari.engineer;
 
+import java.util.HashMap;
+
 public class Main {
     public static void fibonacci(int n) {
         int current = 0;
         int next = 1;
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println(next);
             int temp = current;
             current = next;
@@ -13,28 +15,31 @@ public class Main {
     }
 
     public static int fibonacci_recursion(int n) {
-        if(n == 1) {
-            return 0;
+        if (n == 0 || n == 1) {
+            return n;
         }
-        if (n == 2) {
-            return 1;
-        }
-        return fibonacci_recursion(n-1) + fibonacci_recursion(n-2);
+        return fibonacci_recursion(n - 1) + fibonacci_recursion(n - 2);
     }
 
-    public static void fibonacci_memo(int n) {
-        int[] results = new int[n];
-        results[0] = 0;
-        results[1] = 1;
-        for (int i = 2; i < n; i++) {
-            results[i] = results[i-1] + results[i-2];
+    public static int fibonacci_memo(int n) {
+        return fib_memo(n, new HashMap<>());
+    }
+
+    public static int fib_memo(int n, HashMap<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return n;
         }
-        System.out.println(results[n-1]);
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int result = fib_memo(n - 1, memo) + fib_memo(n - 2, memo);
+        memo.put(n, result);
+        return result;
     }
 
     public static void main(String[] args) {
         fibonacci(10);
         System.out.println(fibonacci_recursion(10));
-        fibonacci_memo(10);
+        System.out.println(fibonacci_memo(10));
     }
 }
