28e4d709e289b77cfb071549fe13e4fc24f88b77
World Ender <hariharansriram17@gmail.com>
2025-03-27 15:42:21 +0000

    add median of two sorted arrays

--- src/main/java/hari/engineer/two/pointer/Main.java
+++ src/main/java/hari/engineer/two/pointer/Main.java
@@ -1,5 +1,7 @@
 package hari.engineer.two.pointer;
 
+import java.util.Arrays;
+
 public class Main {
     public static int trapRainWater(int[] height) {
         int len = height.length;
@@ -24,7 +26,53 @@ public class Main {
         return water;
     }
 
+    public static double medianOfSortedArrays(int[] nums1, int[] nums2) {
+        int[] merged = merge(nums1,nums2);
+        int len = merged.length;
+        int median = 0;
+        System.out.println(len % 2 == 0);
+        if (len % 2 == 0) {
+            return (double) (merged[len / 2] + merged[(len / 2) - 1]) /2;
+        } else  {
+            return merged[(int) (double) (merged.length / 2)];
+        }
+    }
+
+    public static int[] merge(int[] first, int[] second) {
+        int[] resultArr = new int[(first.length + second.length)];
+        int firstLen = first.length;
+        int secondLen = second.length;
+        int i = 0;
+        int j = 0;
+
+        while(i < firstLen && j < secondLen) {
+            if (first[i] <= second[j]) {
+                resultArr[i+j] = first[i];
+                i++;
+            } else {
+                resultArr[i+j] = second[j];
+                j++;
+            }
+        }
+
+        while(i < firstLen) {
+            resultArr[i+j] = first[i];
+            i++;
+        }
+
+        while(j < secondLen) {
+            resultArr[i+j] = second[j];
+            j++;
+        }
+
+        return resultArr;
+    }
+
     public static void main(String[] args) {
-        System.out.println(trapRainWater(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+//        System.out.println(trapRainWater(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+//        System.out.println(medianOfSortedArrays());
+        int[] first = new int[]{1,2};
+        int[] second = new int[]{3,4};
+        System.out.println(medianOfSortedArrays(first, second));
     }
 }
